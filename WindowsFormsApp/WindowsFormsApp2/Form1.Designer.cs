01a2aae6d1459ec7b371ae2bad1f2e14c0aebb02
dark-tulip <darktulip28@gmail.com>
2022-05-29 17:44:25 +0600

    fitst version

---
+++ WindowsFormsApp/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.showEmployees = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.GenerateXML = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Last Names";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(751, 146);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 401);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(220, 134);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // showEmployees
+            // 
+            this.showEmployees.Location = new System.Drawing.Point(801, 12);
+            this.showEmployees.Name = "showEmployees";
+            this.showEmployees.Size = new System.Drawing.Size(192, 40);
+            this.showEmployees.TabIndex = 3;
+            this.showEmployees.Text = "Show Employees";
+            this.showEmployees.UseVisualStyleBackColor = true;
+            this.showEmployees.Click += new System.EventHandler(this.showEmployees_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(21, 198);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(751, 150);
+            this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // GenerateXML
+            // 
+            this.GenerateXML.Location = new System.Drawing.Point(783, 531);
+            this.GenerateXML.Name = "GenerateXML";
+            this.GenerateXML.Size = new System.Drawing.Size(209, 43);
+            this.GenerateXML.TabIndex = 5;
+            this.GenerateXML.Text = "Generate XML";
+            this.GenerateXML.UseVisualStyleBackColor = true;
+            this.GenerateXML.Click += new System.EventHandler(this.GenerateXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 596);
+            this.Controls.Add(this.GenerateXML);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.showEmployees);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button showEmployees;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button GenerateXML;
+    }
+}
+
---
+++ WindowsFormsApp/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Data.Common;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ------------------------------- EXAMPLE 0 ------------------------------- //
+
+            con.Open();
+
+            string sql = "Select EMPLOYEE_ID, LAST_NAME, FIRST_NAME from Employees Order by FIRST_NAME";
+
+            // Создать объект Command.
+            OracleCommand cmd = new OracleCommand();
+
+            // Match Command с Connection.
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        string emp_ID      = reader.GetString(reader.GetOrdinal("EMPLOYEE_ID"));
+                        string empName     = reader.GetString(reader.GetOrdinal("FIRST_NAME"));
+                        string empLastName = reader.GetString(reader.GetOrdinal("LAST_NAME"));
+                        string fullName    = emp_ID + ") " + empName + " " + empLastName;
+
+                        Console.WriteLine("-----------");
+                        Console.WriteLine("EmpName:" + fullName);
+                        textBox1.AppendText(fullName + Environment.NewLine);
+                    }
+                }
+            }
+            MessageBox.Show("Connceted Successful");
+            con.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showEmployees_Click(object sender, EventArgs e)
+        {
+
+            // works with disconnected environment
+            OracleDataAdapter adp = new OracleDataAdapter("Select * from employees", con);
+            
+            // this table stores records
+            DataTable dt = new DataTable();
+
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt.Select("")
+                                          .Where(x => x.Field<int>("employee_id") < 110)
+                                          .CopyToDataTable();
+
+            // ------------------------------- EXAMPLE 1 ------------------------------- //
+
+            // LINQ query on a DataTable
+            var result = dt.Select("")
+                           .FirstOrDefault(myRow => (int)myRow["employee_id"] == 110);
+
+            MessageBox.Show(result["last_name"] + " " + result["first_name"].ToString()); // here can access from any column
+
+            // ------------------------------- EXAMPLE 2 ------------------------------- //
+            var result2 = dt.AsEnumerable()
+                            .Where(myRow => myRow.Field<int>("employee_id") == 100);
+
+            if (result2 != null)
+            {
+                foreach (var res in result2)
+                {
+                    MessageBox.Show(res["employee_id"].ToString());
+                }
+            }
+
+            // ------------------------------- EXAMPLE 3 ------------------------------- //
+            var result3 = dt.AsEnumerable()
+                            .Where(x => x.Field<string>("last_name") == "Chen");
+            if (result3 != null)
+            {
+                foreach (var res in result2)
+                {
+                    MessageBox.Show(res["employee_id"] + " " + res["last_name"]);
+                }
+            }
+
+            // ------------------------------- EXAMPLE 4 ------------------------------- //
+            var result4 = dt.AsEnumerable().ToList();
+            IEnumerable<DataRow> query = from emp in result4 where emp.Field<string>("last_name")
+                                                                      .Contains("Chen") select emp;
+            foreach (DataRow row in query) { 
+                Console.WriteLine(row[0].ToString());
+            }
+
+
+            DataTable dt2 = dt.Select("")
+                              .Where(x => x.Field<int>("employee_id") < 110)
+                              .OrderByDescending(x => x.Field<int>("employee_id"))
+                              .CopyToDataTable();
+
+            dataGridView2.DataSource = dt2;
+
+
+            // ------------------------------- EXAMPLE 5 ------------------------------- //
+            Console.WriteLine("== Some 5  query == ");
+
+
+            string[] selectedColumns = new[] { "first_name", "last_name" };
+            DataTable dt3 = new DataView(dt).ToTable(false, selectedColumns);
+
+            var result5 = dt3.Select("");
+
+            if (result5 != null)
+            {
+                foreach (DataRow row in result5)
+                {
+                    Console.WriteLine(row[0] + " " + row.Field<string>("last_name").ToString());
+                }
+            }
+        }
+
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void GenerateXML_Click(object sender, EventArgs e)
+        {
+
+            // XML File Path
+            const string FILE_PATH = "C:\\Users\\Usernamers\\Desktop\\tansh\\foo3.xml";
+
+            // works with disconnected environment
+            OracleDataAdapter adp = new OracleDataAdapter("select * from departments", con);
+
+            // this table stores records
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+
+            string[] selectedColumns = new[] { "department_id", "department_name" };
+
+            DataTable ToXML_DataTable = new DataView(dt)
+                                    .ToTable(false, selectedColumns)
+                                    .Select("")
+                                    .OrderByDescending(x => x.Field<string>("department_name"))
+                                    .Where(x => x.Field<string>("department_name").Contains("A"))
+                                    .CopyToDataTable();
+
+            ToXML_DataTable.TableName = "DEPARTMENT";    
+
+            new XDocument( new XElement("root") ).Save(FILE_PATH);  // create if not exist
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add(ToXML_DataTable);
+
+            string dsXml = ds.GetXml();       // get XML from data set
+
+            using (System.IO.StreamWriter fs = new System.IO.StreamWriter(FILE_PATH)) 
+            {
+                ds.WriteXml(fs);
+            }
+
+            MessageBox.Show("XML File generated successful!" + Environment.NewLine + "On the file Path: " + FILE_PATH);
+
+        }
+    }
+}
